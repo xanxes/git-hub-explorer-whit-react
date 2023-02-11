eedcf228cc7e0c486a8a75125c5afdfd54875cba
xanxes <oliveirasanchesmarcelo@outlook.com>
2023-02-11 15:18:19 -0300

    subindo

---
+++ src/components/RepositoryList,.jsx
@@ -0,0 +1,29 @@
+import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react"
+
+//https://api.github.com/users/xanxes/repos
+
+
+export function RepositoryList(){
+
+    const [ repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/xanxes/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+            </ul>
+        </section>
+    )
+}
